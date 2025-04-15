0acc64c24b84290b5e801ca120a56411dedef8e2
Evan Sultanik <evan.sultanik@trailofbits.com>
2025-04-15 13:30:43 -0400

    No need for the `strict` option

--- deptective/containers.py
+++ deptective/containers.py
@@ -15,14 +15,12 @@ if sys.version_info >= (3, 12):
 else:
     from itertools import islice
 
-    def batched(iterable, n, *, strict=False):
+    def batched(iterable, n):
         # batched('ABCDEFG', 3) → ABC DEF G
         if n < 1:
             raise ValueError("n must be at least one")
         iterator = iter(iterable)
         while batch := tuple(islice(iterator, n)):
-            if strict and len(batch) != n:
-                raise ValueError("batched(): incomplete batch")
             yield batch
 
 
